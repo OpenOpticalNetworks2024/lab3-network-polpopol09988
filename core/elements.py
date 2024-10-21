58383395b8f888ac0cb3d27d25a5b027ec315dc1
polpopol0 <molino.paolo2001@gmail.com>
2024-10-21 13:04:01 +0200

    lavoro 21/10 mattina

--- core/elements.py
+++ core/elements.py
@@ -1,76 +1,86 @@
 import json
 
 class Signal_information(object):
-    def __init__(self):
-        pass
+    def __init__(self, signal_power, path):
+        self._signal_power = signal_power
+        self._noise_power = 0.0
+        self._latency = 0.0
+        self._path = path if path is not None else []
 
     @property
     def signal_power(self):
-        pass
+        return self._signal_power
 
-    def update_signal_power(self):
-        pass
+    def update_signal_power(self,increment):
+        self._signal_power += increment
 
     @property
     def noise_power(self):
-        pass
+        return self._noise_power
 
     @noise_power.setter
-    def noise_power(self):
-        pass
+    def noise_power(self, value):
+        self._noise_power = value
 
-    def update_noise_power(self):
-        pass
+    def update_noise_power(self,increment):
+        self._noise_power += increment
 
     @property
     def latency(self):
-        pass
+        return self._latency
 
     @latency.setter
-    def latency(self):
-        pass
+    def latency(self,value):
+        self._latency = value
 
-    def update_latency(self):
-        pass
+    def update_latency(self,increment):
+        self.latency += increment
 
     @property
     def path(self):
-        pass
+        return self._path
 
     @path.setter
-    def path(self):
-        pass
+    def path(self,stringa):
+        self._path = stringa
 
     def update_path(self):
-        pass
+        self._path = self._path[1:len(self._path)]
 
 
 class Node(object):
-    def __init__(self):
-        pass
+    def __init__(self,dict,nodo):
+        self._label = nodo
+        self._position = dict[nodo]["position"]
+        self._connected_nodes = dict[nodo]["connected_nodes"]
+        self._successive = dict()
 
     @property
     def label(self):
-        pass
+        return self._label
 
     @property
     def position(self):
-        pass
+        return self._position
 
     @property
     def connected_nodes(self):
-        pass
+        return self._connected_nodes
 
     @property
     def successive(self):
-        pass
+        return self._successive
 
     @successive.setter
-    def successive(self):
-        pass
+    def successive(self,nodo):
+        self._successive = nodo
 
-    def propagate(self):
-        pass
+    def propagate(self,signal):
+        signal.update_path()
+        newpath = signal.path()
+        if newpath != "":
+            nextnode = newpath[0]
+            self.propagate(nextnode)
 
 
 class Line(object):
--- tasks/lab3_network_main.py
+++ tasks/lab3_network_main.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from pathlib import Path
+from core.elements import Signal_information
 
 # Exercise Lab3: Network
 
@@ -15,3 +16,9 @@ file_input = INPUT_FOLDER / 'nodes.json'
 # Then propagate a Signal Information object of 1mW in the network and save the results in a dataframe.
 # Convert this dataframe in a csv file called 'weighted_path' and finally plot the network.
 # Follow all the instructions in README.md file
+
+
+a = Signal_information(10,"ABC")
+a.path = "ciao"
+a.update_path()
+print(a.path)
