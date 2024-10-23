13600764f476b597a71e11941c855de74df43808
polpopol0 <molino.paolo2001@gmail.com>
2024-10-23 11:57:06 +0200

    minor

--- tasks/lab3_network_main.py
+++ tasks/lab3_network_main.py
@@ -12,7 +12,9 @@ from core.elements import Network
 
 ROOT = Path(__file__).parent.parent
 INPUT_FOLDER = ROOT / 'resources'
+OUTPUT_FOLDER = ROOT / 'results'
 file_input = INPUT_FOLDER / 'nodes.json'
+file_output = OUTPUT_FOLDER / 'out.csv'
 
 
 # Load the Network from the JSON file, connect nodes and lines in Network.
@@ -23,6 +25,7 @@ file_input = INPUT_FOLDER / 'nodes.json'
 
 a=Network(file_input)
 a.connect()
+#prove varie in fase di debug
 #for wl in a.lines:
     #print(wl, a.lines[wl].length, a.lines[wl].successive)
 
@@ -31,7 +34,7 @@ a.connect()
 
 #print(a.find_paths('D','B'))
 
-#a.draw()
+a.draw()
 
 #creo possible couples
 lista_nodi = a.nodes.keys()
@@ -42,19 +45,38 @@ for nodo in lista_nodi:
             tmp = nodo+nodo2
             lista_percorsi.append(tmp)
 
-
+pathstring = list()
+latencystring = list()
+noisestring = list()
+snrstring = list()
+stringa = str()
 for percorso in lista_percorsi:
     paths = a.find_paths(percorso[0], percorso[1])
     for path in paths:
-        s = Signal_information(1e-3,path)
+        stringa = str()
+        s = Signal_information(1e-3, path)
         for letter in path[:-1]:
             pass
-            print(letter+'->', end="")
-        print(path[-1], end="")
+            stringa = stringa+letter+"->"
+            #print(letter+'->', end="")
+        #print(path[-1], end="")
+        stringa = stringa+path[-1]
+        pathstring.append(stringa)
         a.propagate(s)
-        print(", latenza: "+ str(s.latency), end ="")
-        print(", noise: "+ str(s.noise_power), end ="")
+        #print(", latenza: " + str(s.latency), end="")
+        latencystring.append(str(s.latency))
+        #print(", noise: " + str(s.noise_power), end="")
+        noisestring.append(str(s.noise_power))
         snr = 10 * np.log10(s.signal_power/s.noise_power)
-        print(", SNR: "+ str(snr))
-
+        #print(", SNR: " + str(snr))
+        snrstring.append(str(snr))
 #manca solo il pandas
+data = {
+    "path": pathstring,
+    "latency": latencystring,
+    "noise_power": noisestring,
+    "SNR": snrstring
+}
+df = pd.DataFrame(data)
+df.to_csv(file_output, index=False)
+
